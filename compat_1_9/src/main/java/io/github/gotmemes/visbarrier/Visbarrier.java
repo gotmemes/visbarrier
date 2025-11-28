db5652d7568e2fafa650634f52742a93b95dbb57
gotmemes <46362694+gotmemes@users.noreply.github.com>
2025-11-27 22:17:34 -0500

    remove compat_1_9

--- compat_1_9/src/main/java/io/github/gotmemes/visbarrier/Visbarrier.java
+++
@@ -1,6 +0,0 @@
-package io.github.gotmemes.visbarrier;
-
-//This is a dummy class
-public class Visbarrier {
-    public static boolean isVisible;
-}
--- compat_1_9/src/main/java/io/github/gotmemes/visbarrier/mixin/SupportedVersions.java
+++
@@ -1,6 +0,0 @@
-package io.github.gotmemes.visbarrier.mixin;
-
-//This is a dummy annotation
-public @interface SupportedVersions {
-    String value();
-}
